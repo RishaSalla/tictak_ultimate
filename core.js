67fd866bc4fdcad435130d38e21e5a61d9162c4e
RishaSalla <salam.kedan@gmail.com>
2026-01-12 13:10:41 +0100

    Update core.js

--- core.js
+++ core.js
@@ -149,3 +149,17 @@ document.querySelectorAll('.lvl-card').forEach(btn => {
 function toggleManual(show) {
     document.getElementById('manual-overlay').classList.toggle('hidden', !show);
 }
+
+checkWin: function(board) {
+    const lines = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // أفقي
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // عمودي
+        [0, 4, 8], [2, 4, 6]             // قطري
+    ];
+    for (let line of lines) {
+        if (board[line[0]] && board[line[0]] === board[line[1]] && board[line[0]] === board[line[2]]) {
+            return board[line[0]];
+        }
+    }
+    return null;
+}
